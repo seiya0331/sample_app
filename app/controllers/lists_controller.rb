431bb32f503a4a3188043b62cc69ab1113362874
SeiyaYamaguchi <141209682+seiya0331@users.noreply.github.com>
2023-08-12 10:19:29 +0000

    [Finish3-11]

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,7 +8,8 @@ class ListsController < ApplicationController
     if @list.save
        redirect_to list_path(@list.id)
     else
-       render :index
+      @lists = List.all
+      render :index
     end
   end
     
